1b83b1b1ca40feb218ca480c6399bfccd6d61b35
wsg <wu5040@gmail.com>
2018-09-23 22:24:20 +0800

    客户端使用用户名和密码登录

--- TCP/Socket_Client/Client.cpp
+++ TCP/Socket_Client/Client.cpp
@@ -1,5 +1,5 @@
 //客户端
-
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,11 +11,112 @@
 #include <arpa/inet.h>
 
 #define MAXLINE 4096
+using namespace std;
+
+int my_recv(int conn_fd, char *data_buf, int len)
+{
+    static char recv_buf[MAXLINE]; // 自定义缓冲区，MAXLINE定义在my_recv.h中
+    static char *pread;            // 指向下一次读取数据的位置
+    static int len_remain = 0;     // 自定义缓冲区中剩余字节数
+    int i;
+
+    // 如果自定义缓冲区中没有数据，则从套接字读取数据
+    if (len_remain <= 0)
+    {
+        if ((len_remain = recv(conn_fd, recv_buf, sizeof(recv_buf), 0)) < 0)
+        {
+            perror("recv");
+        }
+        else if (len_remain == 0)
+        { // 目的计算机端的socket连接关闭
+            return 0;
+        }
+        pread = recv_buf; // 重新初始化pread指针
+    }
+
+    // 从自定义缓冲区中读取一次数据
+    for (i = 0; *pread != '\n'; i++)
+    {
+        if (i > len)
+        { // 防止指针越界
+            return -1;
+        }
+        data_buf[i] = *pread++;
+        len_remain--;
+    }
+
+    // 去除结束标志
+    len_remain--;
+    pread++;
+
+    return i; // 读取成功
+}
+
+void input_userinfo(int sockfd, const char *prompt)
+{
+    char input_buf[32];
+    char recv_buf[MAXLINE];
+    int flag;
+    int count = 0;
+    do
+    {
+        printf("%s:", prompt);
+
+        //获取用户输入存放到input_buf，并做相应处理
+        cin >> input_buf;
+        int len = strlen(input_buf);
+        input_buf[len++] = '\n';
+        input_buf[len] = '\0';
+
+        //将读取到的用户信息发送给服务器
+        if (send(sockfd, input_buf, strlen(input_buf), 0) < 0)
+        {
+            printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
+        }
+
+        //从套接字上读取一次数据
+        if (my_recv(sockfd, recv_buf, sizeof(recv_buf)) < 0)
+        {
+            printf("recv msg error: %s(errno: %d)\n", strerror(errno), errno);
+        }
+
+        if (recv_buf[0] == 'y')
+            flag = 0;
+        else
+        {
+            count++;
+            if (count >= 3)
+            {
+                printf("input error for 3 times, exit!\n");
+                exit(-1);
+            }
+            printf("%s error, input again!\n", prompt);
+            flag = 1;
+        }
+    } while (flag == 1);
+}
+
+void send_msg(int sockfd)
+{
+    char input_buf[MAXLINE];
+    printf("send msg:");
+
+    //获取用户输入存放到input_buf，并做相应处理
+    cin >> input_buf;
+    int len = strlen(input_buf);
+    input_buf[len++] = '\n';
+    input_buf[len] = '\0';
+
+    //将读取到的用户信息发送给服务器
+    if (send(sockfd, input_buf, strlen(input_buf), 0) < 0)
+    {
+        printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
+    }
+}
 
 int main(int argc, char **argv)
 {
 
-    printf("run Client\n");
     int sockfd, recvbytes;
     char recvline[4096], sendline[4096];
     struct sockaddr_in servaddr;
@@ -25,44 +126,56 @@ int main(int argc, char **argv)
         printf("usage: ./client <ipaddress>\n");
         exit(0);
     }
- 
-    char * sevr_sinaddr=argv[1];
-    while (true)
+
+    char *serv_sinaddr = argv[1];
+    // while (true)
+    // {
+
+    //初始化服务器段地址结构
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(6666);
+    if (inet_pton(AF_INET, serv_sinaddr, &servaddr.sin_addr) <= 0)
     {
-        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        {
-            printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
-            exit(0);
-        }
-
-        memset(&servaddr, 0, sizeof(servaddr));
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_port = htons(6666);
-        if (inet_pton(AF_INET, sevr_sinaddr, &servaddr.sin_addr) <= 0)
-        {
-            printf("inet_pton error for %s\n", argv[1]);
-            exit(0);
-        }
-
-        if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-        {
-            printf("connect error: %s(errno: %d)\n", strerror(errno), errno);
-            exit(0);
-        }
-
-        printf("send msg to server: \n");
-        fgets(sendline, 4096, stdin);
-        if (send(sockfd, sendline, strlen(sendline), 0) < 0)
-        {
-            printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
-            exit(0);
-        }
-        
-
-
-        close(sockfd);
+        printf("inet_pton error for %s\n", argv[1]);
+        exit(0);
     }
+
+    //创建TCP套接字
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
+
+    //向服务端发送连接请求
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("connect error: %s(errno: %d)\n", strerror(errno), errno);
+        exit(0);
+    }
+
+    //输入用户名和密码
+    input_userinfo(sockfd, "username");
+    input_userinfo(sockfd, "password");
+
+    //读取登录成功信息并打印出来
+
+    if((recvbytes = my_recv(sockfd, recvline, sizeof(recvline)))<0)
+    {
+        perror("recv error");
+        exit(1);
+    }
+    for(int i=0;i<recvbytes;i++)
+        printf("%c",recvline[i]);
+    printf("\n");
+
+    //登录成功后发送消息
+    send_msg(sockfd);
+
+    close(sockfd);
+    // }
     // exit(0);
 
-    getchar();
+    return 0;
 }
--- TCP/Socket_Server/Server.cpp
+++ TCP/Socket_Server/Server.cpp
@@ -11,6 +11,83 @@
 
 #define MAXLINE 4096
 #define SERVPORT 6666
+#define USERNUM 2
+
+//用户信息：用户名和密码
+struct userinfo
+{
+    char username[32];
+    char password[32];
+};
+struct userinfo users[] = {
+    {"wsg", "123"},
+    {"qqq", "456"}};
+
+
+int my_recv(int conn_fd, char *data_buf, int len)
+{
+    static char recv_buf[MAXLINE]; // 自定义缓冲区，MAXLINE定义在my_recv.h中
+    static char *pread;            // 指向下一次读取数据的位置
+    static int len_remain = 0;     // 自定义缓冲区中剩余字节数
+    int i;
+
+    // 如果自定义缓冲区中没有数据，则从套接字读取数据
+    if (len_remain <= 0)
+    {
+        if ((len_remain = recv(conn_fd, recv_buf, sizeof(recv_buf), 0)) < 0)
+        {
+            perror("recv");
+        }
+        else if (len_remain == 0)
+        { // 目的计算机端的socket连接关闭
+            return 0;
+        }
+        pread = recv_buf; // 重新初始化pread指针
+    }
+
+    // 从自定义缓冲区中读取一次数据
+    for (i = 0; *pread != '\n'; i++)
+    {
+        if (i > len)
+        { // 防止指针越界
+            return -1;
+        }
+        data_buf[i] = *pread++;
+        len_remain--;
+    }
+
+    // 去除结束标志
+    len_remain--;
+    pread++;
+
+    return i; // 读取成功
+}
+
+//验证用户名是否存在
+int check_name(const char *name)
+{
+    int i;
+
+    if (name == NULL)
+    {
+        printf("in check_name, NULL pointer");
+        return -2;
+    }
+    //用户名存在，返回该用户在结构体users中的下标
+    for (i = 0; i < 2; i++)
+        if (strcmp(name, users[i].username) == 0)
+            return i;
+
+    //用户名不存在
+    return -1;
+}
+
+//发送数据
+void send_data(int sockfd,const char* string)
+{
+    if(send(sockfd,string,strlen(string),0)<0)
+        perror("send error");
+}
 
 int main(int argc, char **argv)
 {
@@ -18,30 +95,36 @@ int main(int argc, char **argv)
     struct sockaddr_in servaddr;    //服务端地址信息
     struct sockaddr_in remote_addr; //客户端地址信息
     char recvline[4096], sendline[4096];
+    char recv_buf[MAXLINE];
     int recvbytes;
+    int flag = 0; //0: username; 1: password
+    int user_num;
+    bool LOGGED_IN;
+    pid_t pid;
 
-    //创建socket
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    //创建socket套接字
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
         exit(0);
     }
-    else
-        printf("create socket success\n");
 
+    //设置该套接字使之可以重新绑定端口
+    int optval = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (void *)&optval, sizeof(int));
+
+    //初始化服务器端地址结构
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERVPORT);
 
-    //bind()函数把一个地址族中的特定地址赋给socket
+    //绑定，bind()函数把一个地址族中的特定地址赋给socket
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
     {
         printf("bind socket error: %s(errno: %d)\n", strerror(errno), errno);
         exit(0);
     }
-    else
-        printf("bind socket success\n");
 
     //调用listen()来监听这个socket
     //如果客户端这时调用connect()发出连接请求，服务器端就会接收到这个请求。
@@ -50,34 +133,88 @@ int main(int argc, char **argv)
         printf("listen socket error: %s(errno: %d)\n", strerror(errno), errno);
         exit(0);
     }
-    else
-        printf("listen socket......\n");
 
-    printf("======waiting for client's request======\n");
+    socklen_t sin_sinze = sizeof(struct sockaddr_in);
     while (1)
     {
-        socklen_t sin_sinze = sizeof(struct sockaddr_in);
-        if ((connfd = accept(listenfd, (struct sockaddr *)&remote_addr, &sin_sinze) == -1))
+        LOGGED_IN=false;
+        //accept接受客户端的连接请求，并返回套接字用于收发数据
+        if ((connfd = accept(listenfd, (struct sockaddr *)&remote_addr, &sin_sinze)) == -1)
         {
             printf("accept socket error: %s(errno: %d)", strerror(errno), errno);
             continue;
         }
 
-        printf("received a connection from %s\n", inet_ntoa(remote_addr.sin_addr));
+        printf("accept a connection from %s\n", inet_ntoa(remote_addr.sin_addr));
 
-        recvbytes = recv(connfd, recvline, MAXLINE, 0);
-        if (recvline != "")
+        //创建一个子进程
+        if ((pid = fork()) == 0)
         {
-            recvline[recvbytes] = '\0';
-            printf("recv msg from client: %s\n", recvline);
+            while (1)
+            {
+                //接受客户端发送的用户信息
+                memset(recv_buf, 0, sizeof(recv_buf));
+                if ((recvbytes = recv(connfd, recv_buf, sizeof(recv_buf), 0)) < 0)
+                {
+                    perror("recv error");
+                    exit(1);
+                }
+                recv_buf[recvbytes - 1] = '\0';
+                printf("recv userinfo: %s\n", recv_buf);
+
+                if (flag == 0)
+                {
+                    user_num = check_name(recv_buf);
+                    switch (user_num)
+                    {
+                    case -1:
+                        send(connfd, "n", strlen("n"), 0);
+                        break;
+                    case -2:
+                        exit(1);
+                        break;
+                    default:
+                        send(connfd, "y", strlen("y"), 0);
+                        flag = 1;
+                        break;
+                    }
+                }
+                else if (flag == 1)
+                {
+                    if (strcmp(users[user_num].password, recv_buf) == 0)
+                    {
+                        send_data(connfd,"y\n");
+                        send_data(connfd,"Login Success!\n");
+                        // send(connfd, "y", strlen("y"), 0);
+                        // send(connfd, "Login Success!\n", strlen("Login Success!\n"), 0);
+                        printf("%s login\n", users[user_num].username);
+                        LOGGED_IN = true;
+                        break;
+                    }
+                    else
+                        send(connfd, "n", strlen("n"), 0);
+                }
+            }
+
+            if (LOGGED_IN == true)
+            {
+                //接受客户端发送的消息
+                memset(recvline, 0, sizeof(recvline));
+                if ((recvbytes = recv(connfd, recvline, sizeof(recvline), 0)) < 0)
+                {
+                    perror("recv error");
+                    exit(1);
+                }
+                recvline[recvbytes - 1] = '\0';
+                printf("recv msg: %s\n", recvline);
+            }
+            close(listenfd);
+            close(connfd);
+            exit(0);
         }
-
-        close(connfd);
+        else
+            close(connfd);
     }
-
-    close(listenfd);
-
-    getchar();
 
     return 0;
 }
